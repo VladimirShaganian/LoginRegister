9a8cee0843324d298ff118b9d092c41e7a1c3c5b
vs <vs@gmail.com>
2016-01-17 18:56:43 +0200

    Поправки после синхронизации с сервером

--- system/controller.php
+++ system/controller.php
@@ -6,13 +6,13 @@ class Controller {
     public function view($view, $data = [])
     {
         $view_file = $view . '.php';
-        require_once '/../app/views/' . $view_file;
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/app/views/' . $view_file;
     }
 
     public function model($model)
     {
         $model_file = $model . ".php";
-        require_once "/../app/models/". $model_file;
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/app/models/". $model_file;
         return new $model();
     }
 
--- table.ddl
+++ table.ddl
@@ -2,10 +2,9 @@ CREATE TABLE `users` (
   `id` smallint(3) unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(20) DEFAULT NULL,
   `last_name` varchar(20) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
   `email` varchar(40) DEFAULT NULL,
   `password` varchar(64) DEFAULT NULL,
   `photo` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
